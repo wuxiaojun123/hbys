3ab61fe8c9589cc248c32051fc7ee79ebed98a83
wuxiaojun123 <549661646@qq.com>
2017-02-07 17:14:39 +0800

    add .gitignore

--- HuBangYouShang/app/src/main/java/com/wxj/hbys/App.java
+++ HuBangYouShang/app/src/main/java/com/wxj/hbys/App.java
@@ -5,10 +5,14 @@ import android.app.Application;
 import com.antfortune.freeline.FreelineCore;
 
 /**
+ * 更新一下
+ * <p>
  * Created by wuxiaojun on 2017/1/4.
  */
 
 public class App extends Application {
+
+    public static final String A = "test git commit";
 
     @Override
     public void onCreate() {
