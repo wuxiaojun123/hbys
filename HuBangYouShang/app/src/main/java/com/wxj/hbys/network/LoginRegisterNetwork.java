9c9a450218c9b849680a895c68ba01a7bd7485fe
wuxiaojun1994 <549661646@qq.com>
2017-02-23 08:24:53 +0800

    network activityslideAnim

---
+++ HuBangYouShang/app/src/main/java/com/wxj/hbys/network/LoginRegisterNetwork.java
@@ -0,0 +1,11 @@
+package com.wxj.hbys.network;
+
+/**
+ * Created by wuxiaojun on 2017/2/22.
+ */
+
+public class LoginRegisterNetwork {
+
+
+
+}
---
+++ HuBangYouShang/app/src/main/java/com/wxj/hbys/network/api/LoginApi.java
@@ -0,0 +1,11 @@
+package com.wxj.hbys.network.api;
+
+/**
+ * Created by wuxiaojun on 2017/2/22.
+ */
+
+public interface LoginApi {
+
+
+
+}
